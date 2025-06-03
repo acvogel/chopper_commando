bcce8630b029cfd2cea39b74eed9208c0b605d25
Adam Vogel <acvogel@gmail.com>
2025-06-03 11:03:14 -0700

    Simple bomb dropping and flying

--- chopper.gd
+++ chopper.gd
@@ -2,6 +2,7 @@ extends CharacterBody2D
 
 var speed = 0
 var speed_increment = 50
+@export var bomb_scene: PackedScene
 
 func get_input():
 	var delta_velocity = Vector2(0,0)
@@ -12,9 +13,18 @@ func get_input():
 	if Input.is_action_just_pressed("input_up"):
 		delta_velocity.y -= speed_increment
 	if Input.is_action_just_pressed("input_down"):
-			delta_velocity.y += speed_increment
+		delta_velocity.y += speed_increment
+	if Input.is_action_just_pressed("input_space"):
+		spawn_bomb()
 	return delta_velocity
-			
+	
+func spawn_bomb():
+	var bomb = bomb_scene.instantiate()
+	bomb.linear_velocity = velocity
+	bomb.global_position = global_position
+	bomb.global_position.y += 40.0
+	add_sibling(bomb)
+	
 func _physics_process(delta):
 	var delta_velocity = get_input()
 	velocity += delta_velocity
